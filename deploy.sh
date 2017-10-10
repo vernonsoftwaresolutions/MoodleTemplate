03cfbbf8495b8546c62df2c9097bb4fba5eff940
andrewlarsen <andrew.larsen@concordusa.com>
2017-10-09 21:04:58 -0500

    add     - pip install awscli  for some reason

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 aws s3 cp config.php s3://moodle-deployables 
 aws cloudformation package --template-file moodletemplate.yml --output-template-file moodletemplate-output.yml --s3-bucket moodle-deployables
 aws cloudformation deploy --template-file moodletemplate-output.yml --capabilities CAPABILITY_IAM --STACK_NAME moodle --parameter-overrides Tenant=CompanyA InstanceType=t2.micro DBInstance=moodle DBName=moodle DBIsMultiZone=false KeyName=SANDBOX CertBucket=moodle-deployables HostedZoneName=vssdevelopment.com MoodleDNSName=moodle.vssdevelopment.com
