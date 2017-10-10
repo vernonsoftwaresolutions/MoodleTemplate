0197ca509ccc04496ac101d944dc9b432cca7b0f
andrewlarsen <andrew.larsen@concordusa.com>
2017-10-09 21:10:05 -0500

    fix cli call

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 aws s3 cp config.php s3://moodle-deployables 
 aws cloudformation package --template-file moodletemplate.yml --output-template-file moodletemplate-output.yml --s3-bucket moodle-deployables
-aws cloudformation deploy --template-file moodletemplate-output.yml --capabilities CAPABILITY_IAM --STACK_NAME moodle --parameter-overrides Tenant=CompanyA InstanceType=t2.micro DBInstance=moodle DBName=moodle DBIsMultiZone=false KeyName=SANDBOX CertBucket=moodle-deployables HostedZoneName=vssdevelopment.com MoodleDNSName=moodle.vssdevelopment.com
+aws cloudformation deploy --template-file moodletemplate-output.yml --capabilities CAPABILITY_IAM --stack-name moodle --parameter-overrides Tenant=CompanyA InstanceType=t2.micro DBInstance=moodle DBName=moodle DBIsMultiZone=false KeyName=SANDBOX CertBucket=moodle-deployables HostedZoneName=vssdevelopment.com MoodleDNSName="${PROD_HOST}"
 curl http://"${PROD_HOST}"
 aws cloudformation delete-stack --STACK_NAME "${STACK_NAME}"
