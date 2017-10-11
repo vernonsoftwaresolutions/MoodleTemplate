18e364a2350b4224733e6dc04469be5aaa8e4c9d
andrewlarsen <andrew.larsen@concordusa.com>
2017-10-10 21:44:44 -0500

    change db names

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 aws s3 cp config.php s3://moodle-deployables 
 aws cloudformation package --template-file moodletemplate.yml --output-template-file moodletemplate-output.yml --s3-bucket moodle-deployables
-aws cloudformation deploy --template-file moodletemplate-output.yml --capabilities CAPABILITY_IAM --stack-name "${STACK_NAME}" --parameter-overrides Tenant=CompanyA InstanceType=t2.micro DBInstance=moodle DBName=moodle DBIsMultiZone=false KeyName=SANDBOX CertBucket=moodle-deployables HostedZoneName=vssdevelopment.com. MoodleDNSName="${PROD_HOST}"
+aws cloudformation deploy --template-file moodletemplate-output.yml --capabilities CAPABILITY_IAM --stack-name "${STACK_NAME}" --parameter-overrides Tenant=CompanyB InstanceType=t2.micro DBInstance=moodle1 DBName=moodle1 DBIsMultiZone=false KeyName=SANDBOX CertBucket=moodle-deployables HostedZoneName=vssdevelopment.com. MoodleDNSName="${PROD_HOST}"
 curl http://"${PROD_HOST}"
 aws cloudformation delete-stack --STACK_NAME "${STACK_NAME}"
