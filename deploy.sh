879447dce2862f680fcf56ff6d6f3078c2f28e06
andrewlarsen <andrew.larsen@concordusa.com>
2017-10-11 23:29:39 -0500

    TEST FILE PERMISSION

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 aws s3 cp config.php s3://moodle-deployables 
 aws cloudformation package --template-file moodletemplate.yml --output-template-file moodletemplate-output.yml --s3-bucket moodle-deployables
 aws cloudformation deploy --template-file moodletemplate-output.yml --capabilities CAPABILITY_IAM --stack-name "${STACK_NAME}" --parameter-overrides Tenant=CompanyB InstanceType=t2.micro DBInstance=moodle1 DBName=moodle1 DBIsMultiZone=false KeyName=SANDBOX CertBucket=moodle-deployables HostedZoneName=vssdevelopment.com. MoodleDNSName="${PROD_HOST}"
-curl http://"${PROD_HOST}"
-aws cloudformation delete-stack --STACK_NAME "${STACK_NAME}"
+#curl http://"${PROD_HOST}"
+#aws cloudformation delete-stack --STACK_NAME "${STACK_NAME}"
